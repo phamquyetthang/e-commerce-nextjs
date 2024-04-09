121547250feee2985e3c52c480fab5f0c176a574
phamquyetthang <quyetthang.phucchi@gmail.com>
2024-04-09 22:52:28 +0700

    categories data table

--- src/app/admin/(private_route)/categories/page.tsx
+++ src/app/admin/(private_route)/categories/page.tsx
@@ -1,13 +1,82 @@
+import { ICategory } from "@/app/api/admin/categories/route";
+import { Button } from "@/components/ui/button";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { BASE_URL } from "@/constants/common";
+import { Pencil, Trash2 } from "lucide-react";
 import React from "react";
 
 const Categories = async () => {
   const res = await fetch(`${BASE_URL}/api/admin/categories`);
   const data = await res.json();
-  console.log("🚀 ~ Categories ~ data:", data);
-  const categories = data.data;
 
-  return <div>{JSON.stringify(categories)}</div>;
+  const categories: Array<ICategory> = data.data;
+
+  return (
+    <div className="flex flex-col">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[150px]">Category ID</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Slug</TableHead>
+            <TableHead className="text-right">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {categories.map((category) => (
+            <TableRow key={category.id}>
+              <TableCell className="font-medium">{category.id}</TableCell>
+              <TableCell>{category.name}</TableCell>
+              <TableCell>{category.description}</TableCell>
+              <TableCell>{category.slug}</TableCell>
+              <TableCell className="flex gap-4 justify-end">
+                <Button variant="outline" size="icon">
+                  <Pencil />
+                </Button>
+                <Button variant="outline" size="icon">
+                  <Trash2 />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Pagination className="mx-0 justify-end">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
 };
 
 export default Categories;
